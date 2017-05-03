ef8351de892d75c65be4605ba0cdcbb6238d30a7
Callum McKenna <contact@callum.id.au>
2017-05-03 19:41:07 +1000

    Added navigation menu and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { PeopleComponent } from './people/people.component';
+
+const appRoutes: Routes = [
+  {path: '', component: PeopleComponent},
+  {path: 'people', component: PeopleComponent},
+  {path: 'books', component: BooksComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
